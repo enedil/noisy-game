0fe16ec7de5e557cb1358bc6a01f36cff310991a
enedil <enedil@outlook.com>
2018-05-07 12:59:52 +0200

    Let it be a little more readable
     (and PEP compliant!)

--- noisy-game.py
+++ noisy-game.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 import sys
 
@@ -15,41 +16,41 @@ ROUNDS = 100
 PLAYS = 300
 # results[i][j] = i's score against j.
 def score(players):
-    results = [[0 for _ in range(len(players))] for _ in range(len(players))]
-    for i in range(len(players)):
-        for j in range(i+1):
-            p1 = players[i]
-            p2 = players[j]
-            p1_score = 0
-            p2_score = 0
-            for _ in range(ROUNDS):
-                flip_prob = random.uniform(0, 0.5)
-                p1_plays = []
-                p1_flipped = []
-                p1_state = []
-                p2_plays = []
-                p2_flipped = []
-                p2_state = []
-                for _ in range(PLAYS):
-                    p1_play = p1(p1_plays, p2_flipped, p1_state)
-                    p2_play = p2(p2_plays, p1_flipped, p2_state)
-                    p1_flip = flip(flip_prob, p1_play)
-                    p2_flip = flip(flip_prob, p2_play)
-                    if p1_play == 'c':
-                        p2_score += 2
-                    else:
-                        p1_score += 1
-                    if p2_play == 'c':
-                        p1_score += 2
-                    else:
-                        p2_score += 1
-                    p1_plays.append(p1_play)
-                    p1_flipped.append(p1_flip)
-                    p2_plays.append(p2_play)
-                    p2_flipped.append(p2_flip)
-            # Overwrite if i = j is intentional
-            results[i][j] = p1_score
-            results[j][i] = p2_score
+    results = [[0 for _ in players] for _ in players]
+    for i, j in itertools.combinations_with_replacement(range(len(players)), 2):
+
+        p1 = players[i]
+        p2 = players[j]
+        p1_score = 0
+        p2_score = 0
+        for _ in range(ROUNDS):
+            flip_prob = random.uniform(0, 0.5)
+            p1_plays = []
+            p1_flipped = []
+            p1_state = []
+            p2_plays = []
+            p2_flipped = []
+            p2_state = []
+            for _ in range(PLAYS):
+                p1_play = p1(p1_plays, p2_flipped, p1_state)
+                p2_play = p2(p2_plays, p1_flipped, p2_state)
+                p1_flip = flip(flip_prob, p1_play)
+                p2_flip = flip(flip_prob, p2_play)
+                if p1_play == 'c':
+                    p2_score += 2
+                else:
+                    p1_score += 1
+                if p2_play == 'c':
+                    p1_score += 2
+                else:
+                    p2_score += 1
+                p1_plays.append(p1_play)
+                p1_flipped.append(p1_flip)
+                p2_plays.append(p2_play)
+                p2_flipped.append(p2_flip)
+        # Overwrite if i = j is intentional
+        results[i][j] = p1_score
+        results[j][i] = p2_score
     return results
 
 EVOLUTION_STEPS = 100
